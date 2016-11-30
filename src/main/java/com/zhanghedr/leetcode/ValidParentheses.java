ee28dbd0f662e3fe720fc2d76e076d88b73e13c5
zhanghedr <hezhang0929@gmail.com>
2016-11-30 00:30:18 -0500

    Replace tab to space

--- src/main/java/com/zhanghedr/leetcode/ValidParentheses.java
+++ src/main/java/com/zhanghedr/leetcode/ValidParentheses.java
@@ -16,22 +16,22 @@ import java.util.Stack;
  */
 public class ValidParentheses {
 
-	public static boolean isValid(String s) {
-		if (s == null || s.length() % 2 == 1)
-			return false;
-		Stack<Character> stack = new Stack<>();
-		for (int i = 0; i < s.length(); i++) {
-			char c = s.charAt(i);
-			if (stack.size() > 0) {
-				char peek = stack.peek();
-				if ((peek == '(' && c == ')') || (peek == '[' && c == ']') || (peek == '{' && c == '}')) {
-					stack.pop();
-					continue;
-				}
-			}
-			stack.push(c);
-		}
-		return stack.size() == 0;
-	}
+    public static boolean isValid(String s) {
+        if (s == null || s.length() % 2 == 1)
+            return false;
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (stack.size() > 0) {
+                char peek = stack.peek();
+                if ((peek == '(' && c == ')') || (peek == '[' && c == ']') || (peek == '{' && c == '}')) {
+                    stack.pop();
+                    continue;
+                }
+            }
+            stack.push(c);
+        }
+        return stack.size() == 0;
+    }
 
 }
