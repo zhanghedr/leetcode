64a6b9903a4722dd9575ff25a7ea16777296889f
zhanghedr <hezhang0929@gmail.com>
2016-12-01 00:12:32 -0500

    Replace tab

--- src/main/java/com/zhanghedr/leetcode/RoundRobin.java
+++ src/main/java/com/zhanghedr/leetcode/RoundRobin.java
@@ -16,37 +16,37 @@ import java.util.Queue;
  */
 public class RoundRobin {
 
-	private static class Process {
-		public int reqTime;
-		public int exeTime;
+    private static class Process {
+        public int reqTime;
+        public int exeTime;
 
-		public Process(int reqTime, int exeTime) {
-			this.reqTime = reqTime;
-			this.exeTime = exeTime;
-		}
-	}
+        public Process(int reqTime, int exeTime) {
+            this.reqTime = reqTime;
+            this.exeTime = exeTime;
+        }
+    }
 
-	public static float roundRobin(int[] reqs, int[] exes, int maxExe) {
-		if (reqs == null || exes == null || reqs.length != exes.length)
-			return 0;
-		Queue<Process> queue = new LinkedList<>();
-		int i = 0, curTime = 0, waitTime = 0;
-		while (i < reqs.length || !queue.isEmpty()) {
-			if (!queue.isEmpty()) {
-				Process p = queue.poll();
-				waitTime += curTime - p.reqTime;
-				curTime += Math.min(p.exeTime, maxExe);
-				while (i < reqs.length && reqs[i] <= curTime) {
-					queue.offer(new Process(reqs[i], exes[i]));
-					i++;
-				}
-				if (p.exeTime > maxExe)
-					queue.offer(new Process(curTime, p.exeTime - maxExe));
-			} else {
-				queue.offer(new Process(reqs[i], exes[i]));
-				curTime = reqs[i++];
-			}
-		}
-		return (float) waitTime / reqs.length;
-	}
+    public static float roundRobin(int[] reqs, int[] exes, int maxExe) {
+        if (reqs == null || exes == null || reqs.length != exes.length)
+            return 0;
+        Queue<Process> queue = new LinkedList<>();
+        int i = 0, curTime = 0, waitTime = 0;
+        while (i < reqs.length || !queue.isEmpty()) {
+            if (!queue.isEmpty()) {
+                Process p = queue.poll();
+                waitTime += curTime - p.reqTime;
+                curTime += Math.min(p.exeTime, maxExe);
+                while (i < reqs.length && reqs[i] <= curTime) {
+                    queue.offer(new Process(reqs[i], exes[i]));
+                    i++;
+                }
+                if (p.exeTime > maxExe)
+                    queue.offer(new Process(curTime, p.exeTime - maxExe));
+            } else {
+                queue.offer(new Process(reqs[i], exes[i]));
+                curTime = reqs[i++];
+            }
+        }
+        return (float) waitTime / reqs.length;
+    }
 }
--- src/main/java/com/zhanghedr/leetcode/ShortJobFirst.java
+++ src/main/java/com/zhanghedr/leetcode/ShortJobFirst.java
@@ -17,42 +17,42 @@ import java.util.Queue;
  */
 public class ShortJobFirst {
 
-	private static class Process {
-		public int reqTime;
-		public int exeTime;
+    private static class Process {
+        public int reqTime;
+        public int exeTime;
 
-		public Process(int reqTime, int exeTime) {
-			this.reqTime = reqTime;
-			this.exeTime = exeTime;
-		}
-	}
+        public Process(int reqTime, int exeTime) {
+            this.reqTime = reqTime;
+            this.exeTime = exeTime;
+        }
+    }
 
-	public static float shortJobFirst(int[] reqs, int[] exes) {
-		if (reqs == null || exes == null || reqs.length != exes.length)
-			return 0;
-		Queue<Process> queue = new PriorityQueue<>(new Comparator<Process>() {
-			@Override
-			public int compare(Process p1, Process p2) {
-				if (p1.exeTime == p2.exeTime)
-					return p1.reqTime - p2.reqTime;
-				return p1.exeTime - p2.exeTime;
-			}
-		});
-		int i = 0, curTime = 0, waitTime = 0;
-		while (i < reqs.length || !queue.isEmpty()) {
-			if (!queue.isEmpty()) {
-				Process p = queue.poll();
-				waitTime += curTime - p.reqTime;
-				curTime += p.exeTime;
-				while (i < reqs.length && reqs[i] <= curTime) {
-					queue.offer(new Process(reqs[i], exes[i]));
-					i++;
-				}
-			} else {
-				queue.offer(new Process(reqs[i], exes[i]));
-				curTime = reqs[i++];
-			}
-		}
-		return (float) waitTime / reqs.length;
-	}
+    public static float shortJobFirst(int[] reqs, int[] exes) {
+        if (reqs == null || exes == null || reqs.length != exes.length)
+            return 0;
+        Queue<Process> queue = new PriorityQueue<>(new Comparator<Process>() {
+            @Override
+            public int compare(Process p1, Process p2) {
+                if (p1.exeTime == p2.exeTime)
+                    return p1.reqTime - p2.reqTime;
+                return p1.exeTime - p2.exeTime;
+            }
+        });
+        int i = 0, curTime = 0, waitTime = 0;
+        while (i < reqs.length || !queue.isEmpty()) {
+            if (!queue.isEmpty()) {
+                Process p = queue.poll();
+                waitTime += curTime - p.reqTime;
+                curTime += p.exeTime;
+                while (i < reqs.length && reqs[i] <= curTime) {
+                    queue.offer(new Process(reqs[i], exes[i]));
+                    i++;
+                }
+            } else {
+                queue.offer(new Process(reqs[i], exes[i]));
+                curTime = reqs[i++];
+            }
+        }
+        return (float) waitTime / reqs.length;
+    }
 }
