3c4f1a2fe747013c267f06d11d8203363ca66bda
zhanghedr <hezhang0929@gmail.com>
2016-12-13 00:59:26 -0500

    add problems

--- src/main/java/com/zhanghedr/leetcode/TwoSum.java
+++ src/main/java/com/zhanghedr/leetcode/TwoSum.java
@@ -17,16 +17,16 @@ import java.util.Map;
  */
 public class TwoSum {
 
-	public int[] twoSum(int[] nums, int target) {
-		if (nums == null || nums.length <= 1)
-			return new int[0];
-		Map<Integer, Integer> map = new HashMap<>();
-		for (int i = 0; i < nums.length; i++) {
-			int key = target - nums[i];
-			if (map.containsKey(key))
-				return new int[] { map.get(key), i };
-			map.put(nums[i], i);
-		}
-		return new int[0];
-	}
+    public int[] twoSum(int[] nums, int target) {
+        if (nums == null || nums.length <= 1)
+            return new int[0];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int key = target - nums[i];
+            if (map.containsKey(key))
+                return new int[] { map.get(key), i };
+            map.put(nums[i], i);
+        }
+        return new int[0];
+    }
 }
